aa1e359287d3127576fc6ab7ae3faff2e1e7f21f
modzelpatrycja <modzelp.pat@gmail.com>
2022-06-02 20:35:29 +0200

    Added main window

---
+++ src/main/gui/Main.java
@@ -0,0 +1,27 @@
+package main.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+
+        setSize(1600, 800);
+        setPreferredSize(new Dimension(1600, 800));
+
+        pack();
+        setLocationRelativeTo(null);
+      //  MainPanel p = new MainPanel();
+
+
+    //    this.add(p);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
